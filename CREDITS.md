f69a775b15605b026fa54707f06036555421b469
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-03 10:38:35 -0400

    Send example email

--- CREDITS.md
+++ CREDITS.md
@@ -1,11 +1,5 @@
 # Credits, Notes, and Reference
 
-## GMail API in Python
+## SendGrid
 
-  + https://developers.google.com/gmail/api/quickstart/python (not the most applicable example)
-  + https://github.com/googleapis/google-api-python-client
-  + https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/gspread.md
-  + https://developers.google.com/gmail/api/auth/scopes
-  + https://developers.google.com/gmail/api/guides/sending
-  + https://medium.com/lyfepedia/sending-emails-with-gmail-api-and-python-49474e32c81f
-  + https://stackoverflow.com/questions/43352496/gmail-api-error-from-code-sample-a-bytes-like-object-is-required-not-str
+  + https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/sendgrid.md
--- notification_service.py
+++ notification_service.py
@@ -1,62 +1,38 @@
 
 import os
 from dotenv import load_dotenv
-
-from oauth2client.service_account import ServiceAccountCredentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from email.mime.text import MIMEText
-import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 load_dotenv()
 
-MY_EMAIL_ADDRESS = os.getenv("MY_EMAIL_ADDRESS")
-CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "credentials.json")
-AUTH_SCOPE = ["https://www.googleapis.com/auth/gmail.send"] # Send messages only. No read or modify privileges on mailbox.
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+MY_ADDRESS = os.getenv("MY_ADDRESS")
+#print(MY_ADDRESS, SENDGRID_API_KEY)
+
+client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+print("CLIENT:", type(client))
 
 print("------------------------")
-print("READING CREDENTIALS...")
-print(os.path.isfile(CREDENTIALS_FILEPATH))
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
-#print(type(credentials), credentials) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
-
-print("------------------------")
-print("INITIALIZING THE API CLIENT...")
-client = build("gmail", "v1", credentials=credentials)
-#delegated_credentials = credentials.with_subject(MY_EMAIL_ADDRESS)
-#client = build("gmail", "v1", credentials=delegated_credentials)
-print(type(client)) #> <class 'googleapiclient.discovery.Resource'>
-print(type(client.users())) #> <googleapiclient.discovery.Resource object at ____>
-print(type(client.users().messages())) #> <googleapiclient.discovery.Resource object at ____>
-
-#try:
-#    print("------------------------")
-#    print("GETTING MY PROFILE...")
-#    profile_request = client.users().getProfile(userId="me")
-#    profile_response = profile_request.execute() #> googleapiclient.errors.HttpError: <HttpError 403 when requesting https://www.googleapis.com/gmail/v1/users/me/profile?alt=json returned "Insufficient Permission">
-#    #print("RESPONSE:", type(response))
-#except HttpError as e:
-#    print("OOPS, SOMETHING WENT WRONG", e)
-
-
-print("------------------------")
-print("INITIALIZING THE MESSAGE...")
-message_text = "Grade Details Report\n\n\n..."
-message = MIMEText(message_text)
-message["to"] = MY_EMAIL_ADDRESS # TODO: student address
-message["from"] = MY_EMAIL_ADDRESS
-message["subject"] = "Grade Report - 'Shopping Cart' Project"
-print(message.as_string())
+print("ASSEMBLING MESSAGE CONTENTS...")
+subject = "Grade Details - 'Shopping Cart' Project"
+html_content = "<p>Hello, thanks for submitting your 'Shopping Cart' Project.</p>"
+html_content += "<p>I've evaluated your submission, and the final score is <strong>91.09 / 100</strong>.<p>"
+html_content += "<p>Please see details below for a score break-down by rubric category.</p>"
+html_content += "<p>If you have any questions about your grade, please follow-up within the next few days.</p>"
+html_content += "<p>Thanks, and see you in class!</p>"
+html_content += "<p>- Prof Rossetti</p>"
+message = Mail(from_email=MY_ADDRESS, to_emails=MY_ADDRESS, subject=subject, html_content=html_content)
+print(type(message))
+print(html_content)
 
 try:
     print("------------------------")
-    print("SENDING THE MESSAGE...")
-    #encoded_message = {"raw": base64.urlsafe_b64encode(message.as_string())} #> TypeError: a bytes-like object is required, not 'str'
-    #encoded_message = {"raw": message.as_string()}
-    encoded_message = {"raw": base64.urlsafe_b64encode(message.as_string().encode()).decode()} # h/t: https://stackoverflow.com/a/45306759/670433
-    message_request = client.users().messages().send(userId="me", body=encoded_message)
-    print("REQUEST", type(message_request)) #> <class 'googleapiclient.http.HttpRequest'>
-    message_response = message_request.execute() #> googleapiclient.errors.HttpError: <HttpError 400 when requesting https://www.googleapis.com/gmail/v1/users/me/messages/send?alt=json returned "Invalid value for ByteString: Content-Type: text/plain; charset="us-ascii"
-    print("RESPONSE:", type(message_response)) #> __________
-except:
-    print("OOPS, SOMETHING WENT WRONG")
+    print("SENDING...")
+    response = client.send(message)
+    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+    print(response.status_code) #> 202 indicates SUCCESS
+except Exception as e:
+    print("OOPS, SOMETHING WENT WRONG...", e)
+    print(response.body)
+    print(response.headers)
