1b98bf5d2a211a5d742d90339c6329f5143f7711
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-03 00:54:23 -0400

    Reading credentials

--- notification_service.py
+++ notification_service.py
@@ -1,22 +1,22 @@
 
-# adapted from: https://github.com/s2t2/wip-gmail-py/blob/master/list_messages.py
-
 import os
 
 from googleapiclient.discovery import build
-from oauth2client import file, client, tools
+from oauth2client.service_account import ServiceAccountCredentials
 
+CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "credentials.json")
+AUTH_SCOPE = ["https://www.googleapis.com/auth/gmail.send"] # Send messages only. No read or modify privileges on mailbox.
+
+print("------------------------")
+print("READING CREDENTIALS...")
+print(os.path.isfile(CREDENTIALS_FILEPATH))
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+print(type(credentials), credentials) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
+
+print("------------------------")
 print("INITIALIZING THE API CLIENT...")
-
-credentials_filepath = os.path.join(os.path.dirname(__file__), "auth", "credentials.json")
-print(os.path.isfile(credentials_filepath))
-
-credentials_filestore = file.Storage(credentials_filepath)
-credentials = credentials_filestore.get() #> KeyError: '_module'
-print(type(credentials), credentials)
-
-service = build("gmail", "v1", credentials=creds)
-print(type(service))
+client = build("gmail", "v1", credentials=credentials)
+print(type(client)) #> <class 'googleapiclient.discovery.Resource'>
 
 breakpoint()
 
