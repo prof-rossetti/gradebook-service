8c16a3e3b8051cf2f95670ba51f2c6efc3c2e02a
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-03 02:45:13 -0400

    Getting closer

--- CREDITS.md
+++ CREDITS.md
@@ -11,3 +11,5 @@
   + https://stackoverflow.com/questions/43352496/gmail-api-error-from-code-sample-a-bytes-like-object-is-required-not-str
   + https://stackoverflow.com/questions/43351771/gmail-api-for-python-returns-bad-request
   + https://github.com/googleapis/google-api-python-client/issues/384#issuecomment-293634955
+  + https://stackoverflow.com/a/55144333/670433
+  + https://github.com/s2t2/wip-gmail-py/blob/master/list_messages.py
--- notification_service.py
+++ notification_service.py
@@ -4,22 +4,26 @@ from dotenv import load_dotenv
 
 import base64
 from email.mime.text import MIMEText
-import httplib2
 
-from oauth2client.service_account import ServiceAccountCredentials # flow_from_clientsecrets, FlowExchangeError
-#from oauth2client.file import Storage
-#from oauth2client import file, client, tools
+#from oauth2client.service_account import ServiceAccountCredentials
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-#from google_auth_oauthlib.flow import InstalledAppFlow
-#from google.auth.transport.requests import Request
+
 
 load_dotenv()
 
 MY_EMAIL_ADDRESS = os.getenv("MY_EMAIL_ADDRESS")
-CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "credentials.json")
-AUTH_SCOPES = ["https://www.googleapis.com/auth/gmail.send"] # Send messages only. No read or modify privileges on mailbox.
+#CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "credentials.json")
+CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "auth", "web_creds.json")
+AUTH_SCOPES = [
+    #"https://www.googleapis.com/auth/userinfo.email",
+    #"https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/gmail.send" # Send messages only. No read or modify privileges on mailbox.
+]
 
 print("------------------------")
 print("READING CREDENTIALS...")
@@ -27,52 +31,38 @@ print(os.path.isfile(CREDENTIALS_FILEPATH))
 #credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPES)
 #print(type(credentials), credentials) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
 
-# can't use service account? need refresh token?
-
-#store = Storage(CREDENTIALS_FILEPATH)
-#creds = store.get() #> KeyError: '_module'
-
-#if not creds or not creds.valid:
-#    if creds and creds.expired and creds.refresh_token:
-#        creds.refresh(Request())
-#    else:
-#        flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILEPATH, SCOPES)
-#        creds = flow.run_local_server()
-#    # Save the credentials for the next run
-#    with open('token.pickle', 'wb') as token:
-#        pickle.dump(creds, token)
-
-#if not credentials or credentials.invalid:
-#    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-#    flow.user_agent = APPLICATION_NAME
-#    if flags:
-#        credentials = tools.run_flow(flow, store, flags)
-#    else: # Needed only for compatibility with Python 2.6
-#        credentials = tools.run(flow, store)
-
-#if not creds or creds.invalid:
-#    flow = client.flow_from_clientsecrets('client_id.json', SCOPES) # refers to the credentials file we downloaded
-#    # this is throwing... oauth2client.clientsecrets.InvalidClientSecretsError: Missing property "redirect_uris" in a client type of "web".
-#    # need to re-create the credentials file using "Other UI (Windows, cli)"
-#    # after doing this, you should see Your browser has been opened to visit: https://accounts.google.com/o/oauth2/auth?client_id....
-#    creds = tools.run_flow(flow, store)
-
-
-
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPES)
-#delegated_credentials = credentials.create_delegated(MY_EMAIL_ADDRESS)
-#http = delegated_credentials.authorize(httplib2.Http())
-http = credentials.authorize(httplib2.Http())
-
-service = build("gmail", "v1", http=http) #> oauth2client.client.HttpAccessTokenRefreshError: unauthorized_client: Client is unauthorized to retrieve access tokens using this method, or client not authorized for any of the scopes requested.
 
 
 
 
 
 
-#breakpoint()
+
+
+# create the credential the first time and save it in token.pickle
+creds = None
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILEPATH, AUTH_SCOPES)
+        creds = flow.run_local_server()
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+#create the service
+service = build('gmail', 'v1', credentials=creds)
+
+
+
+
+
+
+
+breakpoint()
 
 
 print("------------------------")
